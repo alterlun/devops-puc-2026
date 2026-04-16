1b1a70c7d6b940a699044f83a07d3458dc082913
Kai <santos.karneiro@gmail.com>
2026-04-15 21:14:45 -0300

    Ajuste 1: Iniciando relatorio

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ dados = {
 
 df = pd.DataFrame(dados)
 
-print("--- Relatório de Vendas ---")
+print("--- Relatório de Vendas V2 ---")
 print(df)
 print("\nMédia de produtos no estoque:", df['Quantidade'].mean())
